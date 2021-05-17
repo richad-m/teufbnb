a39a38330ed7ea8af576a21699deabb876b7e062
Iñaki Gauthier <inakigauthier92@gmail.com>
2021-05-17 15:53:28 +0200

    product controller done

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,40 @@
+class ProductsController < ApplicationController
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+     render :new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(params[product_params])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+end
