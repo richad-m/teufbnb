e0d3f80a73478556a351f1e871bd1d388616d94a
clotilde lacaille <lacailleclotilde@gmail.com>
2021-05-18 11:37:38 +0200

    validation product-user-booking ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many_attached :images
   belongs_to :user
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true
+  validates :images, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], length: { maximum: 6 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 16 }
 end
+
