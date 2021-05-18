2c3aab71f142ae7168a5f1e90cec044816111850
clotilde lacaille <lacailleclotilde@gmail.com>
2021-05-18 14:43:22 +0200

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,5 @@ class Product < ApplicationRecord
   validates :title, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true
-  validates :images, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validates :photos, attached: true
 end
