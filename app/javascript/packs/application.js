4a8c700619fb9d1facca4ba9237ab8697660c1a3
Richad MERALLI <richad.meralli@gmail.com>
2021-05-20 13:15:19 +0200

    add line in index fashboard and javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,12 +29,69 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
-  initUpdateNavbarOnScroll();
+  // initUpdateNavbarOnScroll();
+  // gerer affichage navbar bookings
+  const myStuff = document.querySelector(".my-stuff");
+  const bookings = document.querySelector(".bookings");
+
+  const myStuffBtn = document.querySelector("#mystuff");
+  const bookingsBtn = document.querySelector("#bookings");
+
+  myStuffBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    myStuff.classList.remove('d-none')
+    bookings.classList.add('d-none')
+  });
+
+  bookingsBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    bookings.classList.remove('d-none')
+    myStuff.classList.add('d-none')
+  });
+
 });
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
-
 })
+// Magic navbar in dashboard index page
+$(function() {
+  var $el,
+    leftPos,
+    newWidth,
+    $mainNav = $(".booking .navbar-nav");
+
+  $mainNav.append("<li id='magic-line'></li>");
+  var $magicLine = $("#magic-line");
+
+  $magicLine
+    .width($(".active").width())
+    .css("left", $(".booking .active a").position().left)
+    .data("origLeft", $magicLine.position().left)
+    .data("origWidth", $magicLine.width());
+
+  $(".booking .navbar-nav li a").hover(
+    function() {
+      $el = $(this);
+      leftPos = $el.position().left;
+      newWidth = $el.parent().width();
+      $magicLine.stop().animate({
+        left: leftPos,
+        width: newWidth
+      });
+    },
+    function() {
+      $magicLine.stop().animate({
+        left: $magicLine.data("origLeft"),
+        width: $magicLine.data("origWidth")
+      });
+    }
+  );
+});
+
+// Credit: https://css-tricks.com/jquery-magicline-navigation
+
+
+
